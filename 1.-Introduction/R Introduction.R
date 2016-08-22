e41ba32d558b8dbf2f981d2feb617dd685ed61f4
Anshika Srivastava <a.anshikasrivastava@gmail.com>
2016-08-22 12:00:37 +0530

    R code file
    This R code file is generated on Rstudio with a step by step of introduction concepts examples

---
+++ 1.-Introduction/R Introduction.R
@@ -0,0 +1,34 @@
+#let's add 2 numbers
+5+3
+
+#Let's welcome ourselves to R world shall we?
+"Welcome to R's world user"
+
+#Lets create a variable.
+myFistVariable <- 5 
+
+#this variable is present in R memory. To view it simple type the variabe name in console 
+#or Run the variable name like command
+myFistVariable
+
+#changing value and thus data type of variable
+myFistVariable <- "now it's a string"
+myFistVariable
+
+
+#now let's create a bunch of variables
+height <- 15.3
+width <- 9
+
+#let's list out all the variables that are created? use ls()
+ls()
+
+#Trying to find a variable that doesn't exists?
+area
+
+#removing unused variables from your workspace
+rm(myFistVariable)
+myFistVariable
+
+rm()
+height
